e69d33179a446d5780fd9163c20a7949fe0d54c7
MrVeshij <35579329+MrVeshij@users.noreply.github.com>
2019-03-05 01:23:12 +0300

    complete two tasks

--- task_2.py
+++ task_2.py
@@ -1,12 +1,18 @@
+from collections import Counter
 from tkinter import *
+
 
 
 class AppCalc(Frame):
     """Основной класс приложения, включает в себя следующие методы:
     create_widgets - Отвечает за создание виджетов приложения
-    calculation - Логика расчетов
-    command_calc - Логика работы кнопки
-    main - Метод проверки корректности ввода и преобразования ввода"""
+    ycheck - Контроллер по проверке одинакового количества значений по оси y. Если одинаковых
+    значений встречается нечетное количество, перенаправляет на контроллер проверки xcheck. Если четное
+    запускает метод расчета наличия прямой.(calculation_x)
+    xcheck - Контроллер по проверке одинакового количества значений по оси x. Если одинаковых
+    значений встречается нечетное количество, прямой разделяющей множества нет. Если количество четное
+    запускает метод расчета наличия прямой.(calculation_y)
+    main - Контроллер приложения запускающий вышеуказанную логику"""
 
 
     def __init__(self, master=None):
@@ -20,61 +26,107 @@ class AppCalc(Frame):
         Label(self,
               text = 'Введите через пробел координаты точек.'
               ).grid(row = 0, column = 0, sticky = W)
-        self.numbers = Entry(self)
+        self.numbers = Entry(self,width=40)
         self.numbers.grid(row=1, column=0, sticky=W)
-        self.info = Text(self, width = 15, height = 3, wrap = WORD)
+        self.info = Text(self, width = 30, height = 5, wrap = WORD)
         self.info.grid(row = 7, column = 0, sticky = W)
         Button(self,
                text = 'Результат',
-               command = self.command_calc
+               command = self.main
                    ).grid(row = 6, column = 0, sticky = W)
 
 
-    def calculation(self, numbers):
+    def ycheck(self, numbers):
+        """Проверка четного количества точек по оси y с одинаковыми значениями"""
+        numbers2 = numbers[:]
         points_x = numbers[::2]
-        points_y = list(set(numbers).difference(set(points_x)))
-        list_x = []
-        list_y = []
-        while points_x:
-            x = (min(points_x) + max(points_x))/2
-            points_x.remove(min(points_x))
-            points_x.remove(max(points_x))
-            list_x.append(x)
+        points_y = numbers[::-2]
+        points_y.reverse()
+        check_y = Counter(points_y)
+        sum_y = 0
+        for i in check_y:
+            if check_y.get(i) % 2 != 0:
+                return self.xcheck(numbers, points_x)
+            else:
+                sum_y += 1
+        if sum_y == len(check_y):
+            if self.calculation_y(numbers, check_y) == 0:
+                return self.xcheck(numbers2, points_x)
 
-        while points_y:
-            y = (min(points_y) + max(points_y)) / 2
-            points_y.remove(min(points_y))
-            points_y.remove(max(points_y))
-            list_y.append(y)
 
-        if sum(list_x)/len(list_x) == list_x[0]:
-            print('Yes')
-            return 'Yes'
-        elif sum(list_y)/len(list_y) == list_y[0]:
-            print('Yes')
-            return 'Yes'
+    def xcheck(self, numbers2, points_x):
+        """Проверка четного количества точек по оси x с одинаковыми значениями"""
+        check_x = Counter(points_x)
+        sum_x = 0
+        for k in check_x:
+            if check_x.get(k) % 2 != 0:
+                return 'NO'
+            else:
+                sum_x += 1
+        if sum_x == len(check_x):
+            return self.calculation_x(numbers2, check_x)
+
+
+    def calculation_y(self, numbers, check_y):
+        """Поиск одинаковой точки по оси x"""
+        pairs = []
+        sum_x = 0
+        check_final = []
+        while numbers:
+            pairs.append([numbers[0], numbers[1]])
+            numbers.remove(numbers[1])
+            numbers.remove(numbers[0])
+        for i in check_y:
+            for a in pairs:
+                if a[1] == i:
+                    sum_x += a[0]
+            check_final.append(sum_x/check_y.get(i))
+            sum_x = 0
+        if sum(check_final) / len(check_final) == check_final[0]:
+            return "YES"
         else:
-            print('No')
-            return 'No'
+            return 0
 
 
-    def command_calc(self):
+    def calculation_x(self, numbers, check_x):
+        """Поиск одинаковой точки по оси y"""
+        pairs = []
+        sum_y = 0
+        check_final = []
+        while numbers:
+            pairs.append([numbers[0], numbers[1]])
+            numbers.remove(numbers[1])
+            numbers.remove(numbers[0])
+        for i in check_x:
+            for a in pairs:
+                if a[0] == i:
+                    sum_y += a[1]
+            check_final.append(sum_y/check_x.get(i))
+            sum_y = 0
+        if sum(check_final) / len(check_final) == check_final[0]:
+            return "YES"
+        else:
+            return "NO"
+
+
+    def main(self):
+        """Запуск кнопки"""
         numbers = self.numbers.get()
-        info = self.main(numbers)
-        self.info.delete(0.0, END)
-        self.info.insert(0.0, info)
-
-
-    def main(self, numbers=None):
         if not numbers:
-            numbers = input('Введите координаты точек')
+            info = 'Введите координаты точек.'
+            self.info.delete(0.0, END)
+            self.info.insert(0.0, info)
         numbers = [float(i) for i in numbers.split(' ')]
         amount_numbers = len(numbers)
         if amount_numbers % 2 != 0 or amount_numbers in range(2,1000,4):
-            print('Укажите четное количество точек.')
-            return 'Укажите четное количество точек.'
+            info = 'Укажите четное количество точек.'
+            self.info.delete(0.0, END)
+            self.info.insert(0.0, info)
         else:
-            return self.calculation(numbers)
+            info = self.ycheck(numbers)
+            self.info.delete(0.0, END)
+            self.info.insert(0.0, info)
+
 
 
 def main_app():
